834e028390304059e670b26c73939a4a6576041f
노우석 <nhowooseok@gmail.com>
2022-10-26 09:54:23 +0900

    코드정리

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,4 @@
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from articles import serializers
 from articles.models import Article
 from articles.serializers import ArticleSerializer
 from rest_framework.decorators import api_view
